bed6673ec3449edee0bf8515bae30cca4599d389
AyazKemal <kemayaz@gmail.com>
2023-05-16 10:10:14 +0300

    Add project files.

---
+++ KmlOnline.Presentation/Abstraction/ApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;// install from
+
+namespace KmlOnline.Presentation.Abstraction;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiController : ControllerBase
+{
+}
---
+++ KmlOnline.Presentation/Controller/TestController.cs
@@ -0,0 +1,12 @@
+﻿using KmlOnline.Presentation.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KmlOnline.Presentation.Controller;
+public sealed class TestController : ApiController
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("İşlem Başarılı");
+    }
+}
